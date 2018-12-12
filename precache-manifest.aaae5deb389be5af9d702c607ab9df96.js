a538223a5fd0a05b032568b079d344b94a2420c2
h-cal <calvinho.ca@gmail.com>
2018-12-12 11:51:00 +0800

    Updates

---
+++ precache-manifest.aaae5deb389be5af9d702c607ab9df96.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "0546ca5d7ac3604fcf50",
+    "url": "/clicky-game/static/css/main.74c459df.chunk.css"
+  },
+  {
+    "revision": "0546ca5d7ac3604fcf50",
+    "url": "/clicky-game/static/js/main.0546ca5d.chunk.js"
+  },
+  {
+    "revision": "5b265051865492349dc6",
+    "url": "/clicky-game/static/js/1.5b265051.chunk.js"
+  },
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "969910ab060bc75533fe0c9277332f8d",
+    "url": "/clicky-game/static/media/logo.969910ab.png"
+  },
+  {
+    "revision": "14ee7c2688b2d2f53ae9d4c66a6dd778",
+    "url": "/clicky-game/static/media/gary.14ee7c26.png"
+  },
+  {
+    "revision": "6410ca4f7e6079852991dc8fc66c6590",
+    "url": "/clicky-game/static/media/karen.6410ca4f.png"
+  },
+  {
+    "revision": "9a9d6b0ec76f68f1d3432bf27ca12596",
+    "url": "/clicky-game/static/media/larry.9a9d6b0e.png"
+  },
+  {
+    "revision": "507476a94f1e1198aac147a6e548f348",
+    "url": "/clicky-game/static/media/mrkrabs.507476a9.png"
+  },
+  {
+    "revision": "06d479d42187245bc32aff62ddef3557",
+    "url": "/clicky-game/static/media/patrick.06d479d4.png"
+  },
+  {
+    "revision": "d1fd4fa47f78f27993252ccf4678d882",
+    "url": "/clicky-game/static/media/pearl.d1fd4fa4.png"
+  },
+  {
+    "revision": "7e381354ae0cab225096ff95b73342fb",
+    "url": "/clicky-game/static/media/perkins.7e381354.png"
+  },
+  {
+    "revision": "578771f390405e42f16f1dc8eb42e24a",
+    "url": "/clicky-game/static/media/plankton.578771f3.png"
+  },
+  {
+    "revision": "1fe421ddf139e88f6b40b3899176d94b",
+    "url": "/clicky-game/static/media/puff.1fe421dd.png"
+  },
+  {
+    "revision": "615a6e8c759988021dad1c370912c716",
+    "url": "/clicky-game/static/media/sandy.615a6e8c.png"
+  },
+  {
+    "revision": "eddb19377ae432ba786f100e91762ec0",
+    "url": "/clicky-game/static/media/spongebob.eddb1937.png"
+  },
+  {
+    "revision": "37f670029946c0a6d71c5684f6fadc41",
+    "url": "/clicky-game/static/media/squidward.37f67002.png"
+  },
+  {
+    "revision": "f43ab49c2b96740168869d7dc780b916",
+    "url": "/clicky-game/index.html"
+  }
+];
